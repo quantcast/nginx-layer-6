1ab416a1fa023f45e7eeb8123a9be70ed8669f84
Anshul Kamath <anshulkamath@users.noreply.github.com>
2023-04-20 14:22:50 -0700

    Implement: Request Pipelining
    Implemented support for request pipelining. Pipelined requests are processed as
    they are received, and will be sent as soon as a full request is found. Support
    is in place for both pipelined GET and POST requests.

--- httplite_request.h
+++ httplite_request.h
@@ -6,41 +6,44 @@
 #include <ngx_event.h>
 
 #include "httplite_upstream.h"
+#include "httplite_request_list.h"
 
-#define SLAB_SIZE 1500                              /* MTU size */
+enum HTTP_method { GET, POST };
 
-/**
- * @returns new httplite linked list of slabs, where each slab contains a
- * pointer to a SLAB_SIZE string buffer, using the given connection.
-*/
-httplite_request_list_t *httplite_init_list(ngx_connection_t *connection);
+/* Data about the connection that is maintained between calls to the request handler*/
+typedef struct httplite_client_data_s {
+    httplite_request_list_t *read_list;             /* Where the raw request strings are received into */
+    httplite_request_list_t *write_list;            /* Head of the list where each node is a separate request */
 
-void httplite_free_list(httplite_request_list_t *list);
+    /* The following are utility fields used by request splitting */
+    httplite_request_list_t *write_list_tail;       /* Tail node of the chain of parsed requests */
+    httplite_request_list_t *staging_list;          /* Intermediate holding area for fragmented headers */
+    size_t bytes_remaining;                         /* For the current request, how many bytes are left to be copied */
+    size_t step_number;                             /* Keeps track of which step in the request parsing we are on */
+    size_t pending_read_slabs;                      /* If this gets too large, shuts down the connection */
+} httplite_request_data_t;
 
-/**
- * Adds a node to the given list given the httplite_request_list
- * 
- * @returns A pointer to the new slab in the list
-*/
-httplite_request_slab_t *httplite_add_slab(httplite_request_list_t *list);
-
+/* ------------------------------------------------------------------------------
+                        Connection and upstream functions
+   ---------------------------------------------------------------------------- */
 void httplite_request_handler(ngx_event_t *rev);
 void httplite_close_connection(ngx_connection_t *c);
+void httplite_send_request_list(httplite_request_data_t *request_data);
 
+/* ------------------------------------------------------------------------------
+                            Request splitting function
+   ---------------------------------------------------------------------------- */
 /**
- * Given a slab, looks at the buffer (assumed to contain all the headers of a request)
- * 
- * @returns Total request length = size of headers + size of body 
+* given a request_data, parses request_data->read_list into separate requests
+* populates them as nodes in write_list
+* there will always be one empty node at the end of write list
 */
-ssize_t find_request_length(httplite_request_slab_t *slab);
+void httplite_split_request(httplite_request_data_t *request_data, ngx_connection_t *c);
 
-/**
- * Reads from a connection into a slab buffer (overwrites contents)
- * 
- * @returns Number of bytes read
-*/
-size_t recv_wrapper(ngx_connection_t *c, httplite_request_list_t *request, ngx_event_t *rev);
-
-void httplite_client_handle_wakeup(ngx_event_t *event);
+/* ------------------------------------------------------------------------------
+                                Testing functions
+   ---------------------------------------------------------------------------- */
+void httplite_print_requests (httplite_request_list_t *requests);
+void httplite_print_request (httplite_request_list_t *request);
 
 #endif
