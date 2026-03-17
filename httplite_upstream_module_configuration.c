a29074d0c8500dbfe0de31cdceb4187e03dd5f98
Scott S. McCoy <scott.s.mccoy@acm.org>
2026-03-17 13:54:19 -0700

    config: verify alloc before dereference

--- httplite_upstream_module_configuration.c
+++ httplite_upstream_module_configuration.c
@@ -47,11 +47,11 @@ httplite_create_upstream_configuration(ngx_conf_t *cf)
     }
 
     cucf->connection_pool = ngx_pcalloc(cf->pool, sizeof(httplite_connection_pool_t));
-    cucf->connection_pool->pool = cf->pool;
     if (!cucf->connection_pool) {
         fprintf(stderr, "Unable to allocate space for connection pool.\n");
         return NULL;
     }
+    cucf->connection_pool->pool = cf->pool;
 
     cucf->connection_pool->upstream_pools = ngx_array_create(cf->pool, 4, sizeof(httplite_upstream_pool_t));
     cucf->connection_pool->pool_index = 0;
